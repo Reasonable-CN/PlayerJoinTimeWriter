65076ea7780159665691ba999a9cc776c95577af
qfys521 <qfys520@vip.qq.com>
2023-04-28 18:29:32 +0800

    fix bug #1

--- src/main/java/cn/qfys521/playerjointimewriter/PlayerJoinTimeWriter.java
+++ src/main/java/cn/qfys521/playerjointimewriter/PlayerJoinTimeWriter.java
@@ -1,17 +1,63 @@
 package cn.qfys521.playerjointimewriter;
 
+import cn.qfys521.QfToolKit.DataUtil.yamlUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlayerJoinTimeWriter extends JavaPlugin {
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+
+public final class PlayerJoinTimeWriter extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new PlayerJoinTimeWriterL(), this);
+        getLogger().info("插件启动中。。。。。");
+        getLogger().info("作者: qfys521");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("插件卸载中。。。。。");
+        getLogger().info("感谢您的使用。");
     }
+
+    @Override
+    @SuppressWarnings("all")
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length != 1) {
+            sender.sendMessage("\u00a76您的参数输入有误，您输入了\u00a74 " + args.length + " \u00a76个参数，但是该命令仅仅接受 \u00a7b1\u00a76 个参数。\u00a7r");
+        } else if (sender.hasPermission("getPlayerJoinTime.getter") || command.getName().equalsIgnoreCase("getPlayerLastJoinTime") || command.getName().equalsIgnoreCase("gpljt")) {
+            try {
+                Plugin plugin = Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("PlayerJoinTimeWriter"));
+                try {
+                    String get = (String) plugin.getConfig().get(args[0]);
+                    if (get!=null) {
+                        sender.sendMessage("玩家 \u00a7e" + args[0] + " \u00a7r上次在线时间为: \u00a7b" + get + "\u00a7r");
+                    }else {
+                        sender.sendMessage("玩家 \u00a7e" + args[0] + " \u00a7r\u00a7l\u00a7d从未\u00a7r 加入过服务器");
+                    }
+                }catch (Exception e){
+                    sender.sendMessage("\u00a74发生了异常，请联系管理员。\u00a7r");
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                sender.sendMessage("\u00a74发生了异常，请联系管理员。\u00a7r");
+                e.printStackTrace();
+            }
+            return true;
+        }
+        return false;
+    }
+
+
 }
+
--- src/main/java/cn/qfys521/playerjointimewriter/PlayerJoinTimeWriterL.java
+++ src/main/java/cn/qfys521/playerjointimewriter/PlayerJoinTimeWriterL.java
@@ -1,33 +1,34 @@
 package cn.qfys521.playerjointimewriter;
 
 import cn.qfys521.QfToolKit.DataUtil.yamlUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
+
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
-public class PlayerJoinTimeWriterL extends JavaPlugin implements Listener {
+public class PlayerJoinTimeWriterL implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onLogin(PlayerLoginEvent event){
+    public void onLogin(PlayerLoginEvent event) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat();// 格式化时间
             sdf.applyPattern("yyyy-MM-dd HH:mm:ss a");// a为am/pm的标记
             Date date = new Date();// 获取当前时间
+            Plugin plugin = Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("PlayerJoinTimeWriter"));
+            plugin.getConfig().set(event.getPlayer().getName(),sdf.format(date));
+            plugin.saveConfig();
 
-            yamlUtil yaml = new yamlUtil();
-            yaml.setPath(getConfig().getCurrentPath());
-
-            Map<Object,Object> map = new HashMap<>();
-            map.put(event.getPlayer().getName(),sdf.format(date));
-
-            yaml.write(map);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
